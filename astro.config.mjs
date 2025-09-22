0784414899ea95dc7562ac8fff2471ca1234382f
Mitsuo Koikawa <mkoikawa@tonoi.co.jp>
2025-09-22 09:47:20 +0900

    Astro出力先を/docsに変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   site: 'https://tonoi.co.jp',
   output: 'static',
+  outDir: './docs',
   integrations: [
     tailwind({
       applyBaseStyles: false
